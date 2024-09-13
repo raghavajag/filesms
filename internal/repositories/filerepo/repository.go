083643b99fc7bc7fe2c07394fdf70bb5939a913e
raghavajag <raghavajagarwal@gmail.com>
2024-09-13 22:07:54 +0530

    defining user, file repositories

--- internal/repositories/filerepo/repository.go
+++ internal/repositories/filerepo/repository.go
@@ -1,1 +1,13 @@
 package filerepo
+
+import (
+	"database/sql"
+)
+
+type postgresFileRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresFileRepository(db *sql.DB) *postgresFileRepository {
+	return &postgresFileRepository{db: db}
+}
--- internal/repositories/userrepo/repository.go
+++ internal/repositories/userrepo/repository.go
@@ -1,1 +1,13 @@
 package userrepo
+
+import (
+	"database/sql"
+)
+
+type postgresUserRepository struct {
+	db *sql.DB
+}
+
+func NewPostgresUserRepository(db *sql.DB) *postgresUserRepository {
+	return &postgresUserRepository{db: db}
+}
