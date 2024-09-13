df69f9374d86eb495de7bdd39166168760132174
raghavajag <raghavajagarwal@gmail.com>
2024-09-13 21:43:53 +0530

    defining ports and domain for user, file

--- internal/core/domain/file.go
+++ internal/core/domain/file.go
@@ -1,1 +1,14 @@
 package domain
+
+import "time"
+
+type File struct {
+	ID        uint      `json:"id"`
+	UserID    uint      `json:"user_id"`
+	Name      string    `json:"name"`
+	Size      int64     `json:"size"`
+	Type      string    `json:"type"`
+	URL       string    `json:"url"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
